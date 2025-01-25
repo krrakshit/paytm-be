8b010a26d10de8dcf58f086ae835401f57d8b9cc
krrakshit <rkrkr365@gmail.com>
2025-01-25 16:45:07 +0530

    database and routes

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 module.exports = {
-	JWT_SECRET: "ee375655511"
+	JWT_SECRET: "process.env.JWT_SECRET"
 }
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
+import rootRouter from "./routes";
 const express = require("express");
+const cors = require("cors")
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1",rootRouter)
+app.listen(3000)
+
 
 
